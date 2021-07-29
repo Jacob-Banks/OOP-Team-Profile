362ea4c1e86852ee670921a89aa92e9c37c1f733
Jacob Banks <kobybanks@gmail.com>
2021-07-29 13:33:27 -0400

    prompts to fill team array with constructor objects

---
+++ index.js
@@ -0,0 +1,109 @@
+// node modules
+const inquirer = require("inquirer");
+
+// employee classes
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+//team array
+const team = [];
+
+const prompts = (role) => {
+  let unique;
+  switch (role) {
+    case "Manager":
+      unique = "Office";
+      break;
+    case "Engineer":
+      unique = "GitHub";
+      break;
+    case "Intern":
+      unique = "School";
+      break;
+  }
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: `Enter ${role}'s name.`,
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter ${role}'s name!`);
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: `Enter ${role}'s ID.`,
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(`Please enter ${role}'s ID!`);
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: `Enter ${role}'s email.`,
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log(`Please enter ${role}'s email!`);
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "unique",
+        message: `Enter ${role}'s ${unique}.`,
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(`Please enter ${role}'s ID!`);
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "nextEmployee",
+        message: "What type of employee would you like to add to your team?",
+        choices: ["Engineer", "Intern", "Finissheed building team"],
+      },
+    ])
+    .then((data) => {
+      const { name, id, email, unique, nextEmployee } = data;
+      switch (role) {
+        case "Manager":
+          const manager = new Manager(name, id, email, unique);
+          team.push(manager);
+          break;
+        case "Engineer":
+          const engineer = new Engineer(name, id, email, unique);
+          team.push(engineer);
+          break;
+        case "Intern":
+          const intern = new Intern(name, id, email, unique);
+          team.push(intern);
+          break;
+      }
+
+      if (nextEmployee === "Engineer") {
+        return prompts("Engineer");
+      } else if (nextEmployee === "Intern") {
+        return prompts("Intern");
+      } else {
+        console.log("finished", team);
+      }
+    });
+};
+prompts("Manager");
